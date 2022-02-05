54782bd50010e158c5eb6557564129bad1d525b3
Prajwal Koirala <64820837+prajwal-skdlove@users.noreply.github.com>
2022-02-05 10:22:25 -0500

    Update R_WordleHelper.R

--- R_WordleHelper.R
+++ R_WordleHelper.R
@@ -1,16 +1,21 @@
+library(data.table)
+library(qdapDictionaries)
+
+# Get a list of acceptable words
+# For this grab a dictionary for all 5 letter words
+n = 5
+prm = rbind(data.table(DICTIONARY)[,1], data.table(word = GradyAugmented))
+prm = prm[nchar(word) == n]
+prm = unique(prm, by = "word")
+
+
 # Function to help solve wordle
-function_wordle <- function(n,e,is,ns){
-  library(data.table)
-  library(qdapDictionaries)
-  
-  # Create a list of n letter words
-  prm = rbind(data.table(DICTIONARY)[,1], data.table(word = GradyAugmented))
-  prm = prm[nchar(word) == n]
-  prm = unique(prm, by = "word")
+function_wordle <- function(df,e,is,ns){
+
   
   # Exclude letters not in the word
   ptrn_excl = paste0(e, collapse="|")
-  prm = prm[!grepl(ptrn_excl, word, perl = TRUE)]
+  df = df[!grepl(ptrn_excl, word, perl = TRUE)]
   
   # Keep words with letters already guessed in only the right spot
   is1 = is[!(is.na(is))]
@@ -18,13 +23,13 @@ function_wordle <- function(n,e,is,ns){
     m = match(is1, is)
     for (j in m){
       ptrn = paste0("^.{", j - 1,"}[",is[j],"]", collapse="")
-      prm = prm[grepl(ptrn, word, perl=TRUE)]
+      df = df[grepl(ptrn, word, perl=TRUE)]
   
       # Cant do this as repeated words in the spot 
       # would not always be be figured out
       # for (z in seq(1:5)[!(seq(1:5) %in% m)]){
       #   ptrnz = paste0("^.{", z - 1,"}[",is[j],"]", collapse="")
-      #   prm = prm[!(grepl(ptrnz, word, perl=TRUE))]
+      #   df = df[!(grepl(ptrnz, word, perl=TRUE))]
       # }
     }
   }
@@ -35,22 +40,20 @@ function_wordle <- function(n,e,is,ns){
   ns1 = ns[!is.na(ns)]
   if (length(ns1) > 0){
     ptrn2 = paste0("(?=.*",ns1,")", collapse="")
-    prm = prm[grepl(ptrn2, word, perl = TRUE)]  #keep only words with letters
+    df = df[grepl(ptrn2, word, perl = TRUE)]  #keep only words with letters
     
     m1 = match(ns1, ns)
     for (k in m1){
       ptrn3 = paste0("^.{", k - 1,"}[",ns[k],"]", collapse="")
-      prm = prm[!(grepl(ptrn3, word, perl=TRUE))]
+      df = df[!(grepl(ptrn3, word, perl=TRUE))]
     }
   }
 
-  return(prm)
+  return(df)
 }
 
-
-excl = c("r","a", "i", "n", "l", "p") #letters that aren't in
-in_seq = c(NA,NA,"o",NA, "e") #letters in the right spot
-in_notseq = c("s", NA,NA, NA, NA) #letter not in the right spot
-no = 5
-wordle_list = function_wordle(no, excl, in_seq, in_notseq)
+excl = c("r","i", "n", "e", "u", "d") #letters that aren't in
+in_seq = c("a","l","o",NA, NA) #letters in the right spot
+in_notseq = c(NA, "a",NA, NA, NA) #letter not in the right spot
+wordle_list = function_wordle(prm, excl, in_seq, in_notseq)
 
