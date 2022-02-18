5bbb7683ce427691af840ed009f8cd6997708bc8
Prajwal Koirala <64820837+prajwal-skdlove@users.noreply.github.com>
2022-02-18 17:15:13 -0500

    Update R_Wordguess.R
    Updated the full code with probability

--- R_Wordguess.R
+++ R_Wordguess.R
@@ -1,10 +1,13 @@
 # Basically a copy of Wordle. Picks a word and guesses it at random
 # Uses the wordle helper function
 
+# Basically a copy of Wordle. Picks a word and guesses it at random
+# Uses the Wordle helper function
+
 
 # Function to help solve wordle
 function_wordle <- function(df,e,is,ns){
-
+  
   
   # Exclude letters not in the word
   if (length(e) > 0) {
@@ -18,7 +21,7 @@ function_wordle <- function(df,e,is,ns){
     for (j in m){
       ptrn = paste0("^.{", j - 1,"}[",is[j],"]", collapse="")
       df = df[grepl(ptrn, word, perl=TRUE)]
-  
+      
       # Cant do this as repeated words in the spot 
       # would not always be be figured out
       # for (z in seq(1:5)[!(seq(1:5) %in% m)]){
@@ -27,8 +30,8 @@ function_wordle <- function(df,e,is,ns){
       # }
     }
   }
-
-
+  
+  
   # keep words with letters already guessed but not at the right spot
   # eliminate words with letter on the wrong spot
   ns1 = ns[!is.na(ns)]
@@ -42,20 +45,34 @@ function_wordle <- function(df,e,is,ns){
       df = df[!(grepl(ptrn3, word, perl=TRUE))]
     }
   }
-
+  
   return(df)
+}
+
+function_wrdprob = function(x, var, is,ns){
+  ls = unique(c(is,ns))
+  strng = unique(unlist(strsplit(x,"")))
+  ptrn_cnt = paste0(strng[!(strng %in% ls)], collapse="|")
+  prob = round(sum(grepl(ptrn_cnt, var, perl = TRUE)) / length(var),4)
+  return(prob)
 }
 
 
 word_guess = function(wl){
-  word_of_the_day = unlist(wl[sample(nrow(wl),1)]) #word to guess
-    
+  colnames(wl) = c("word", "prob")
+  word_of_the_day = wl$word[sample(length(wl$word),1)] #word to guess
+  # word_of_the_day = "dodge"
+  
+  
   # Set a counter and make the first attempt
-  # Guess at random
+  # Guess based on higher prob or may be this should be YOLO
+  # Do the first one at random - Not effective
+  # Doing it based on probability better
   counter = 1
-  attempt = unlist(wl[sample(nrow(wl),1)])
+  attempt = wl[ prob == max(prob)]$word
+  attempt = attempt[sample(length(attempt),1)]
   attempted_words = c()
-   
+  
   # Set some variable to keep guessing the word until you get it right
   # Utilize the wordle_function created
   n = nchar(word_of_the_day)
@@ -63,16 +80,16 @@ word_guess = function(wl){
   in_notseq = rep(NA,n)
   excl = c()
   
-  # Keep trying until you gess the word
+  # Keep trying until you guess the word
   while (word_of_the_day != attempt){
     # print(paste(paste0("Attempt ", counter), attempt, sep = " = "))
     
     # For the word guessed, check whether there are any matched words
     # Either in the right spot or the wrong spot
     # Generate words to be excluded and words in seq and not in seq
-
-    for (k in 1:n){
      
+    for (k in 1:n){
+      
       subst_word = substr(word_of_the_day,k,k)
       substr_attempt = substr(attempt,k,k)
       atmpt_wrd_ptrn = paste0("(?=.*",substr_attempt,")", collapse="")
@@ -80,31 +97,45 @@ word_guess = function(wl){
       if (subst_word == substr_attempt){
         in_seq[k] = substr_attempt
       } else if (subst_word != substr_attempt & 
-          grepl(atmpt_wrd_ptrn, word_of_the_day, perl = TRUE)){
-            in_notseq[k] = substr_attempt
-          } else {
-            excl = c(excl, substr_attempt)
-          }
-        
+                 grepl(atmpt_wrd_ptrn, word_of_the_day, perl = TRUE)){
+        in_notseq[k] = substr_attempt
+      } else {
+        excl = c(excl, substr_attempt)
+      }
+      
     }
-
+    
     # Run the World function to eliminate words
     wl = function_wordle(wl, excl, in_seq, in_notseq)
     
     # Keep a lit of attempted words
     attempted_words = c(attempted_words, attempt)
-    wl = wl[!(wl$word %in% attempted_words),]
+    wl = wl[!(wl$word %in% attempted_words),c("word")]
+    
+    # Find which word is the best to guess
+    wl = data.table(cbind(word = wl$word, 
+                          prob = sapply(unlist(wl$word),
+                                        function(x)function_wrdprob(x,
+                                                                    wl$word,
+                                                                    in_seq,
+                                                                    in_notseq),
+                                        simplify = TRUE,
+                                        USE.NAMES = TRUE)))
+  
+
+
     
     # Try another attempt
-    attempt = unlist(wl[sample(nrow(wl),1)])
+    attempt = wl[ prob == max(prob)]$word
+    attempt = attempt[sample(length(attempt),1)]    
     counter = counter + 1
   }
   
-   print("SUCCESS!")
-   print(paste0("Attempt ", counter))
-   print(paste0("Word of the Day is ", attempt))
-   
-   return(counter)
+  print("SUCCESS!")
+  print(paste0("Attempt ", counter))
+  print(paste0("Word of the Day is ", attempt))
+  
+  return(counter)
   #return(list(counter, attempted_words))
 }
 
@@ -120,5 +151,22 @@ prm = rbind(data.table(DICTIONARY)[,1], data.table(word = GradyAugmented))
 prm = prm[nchar(word) == n]
 prm = unique(prm, by = "word")
 
+# Find which word is the best to guess outside of the function
+prm = data.table(cbind(word = prm$word, 
+                        prob = sapply(unlist(prm$word),
+                                      function(x)function_wrdprob(x,
+                                                                  prm$word,
+                                                                  NA,
+                                                                  NA),
+                                      simplify = TRUE,
+                                      USE.NAMES = TRUE)))
 
-word_guess(prm)
+# Run the Code 500 time
+replicate(500,word_guess(prm))
+
+# Use this for helper to solve Wordle 
+wordle_list = prm
+excl = c( "h", "o", "k", "e","i", "a", "r", "s",  "w", "n") 
+in_seq = c("c", "l" ,NA,"m" , NA)
+in_notseq = c(NA, "p" ,NA, "c", NA)
+wordle_list = function_wordle(wordle_list, excl, in_seq, in_notseq)
